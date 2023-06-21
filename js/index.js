dac7fb6c779a16639b11e185a0e68ba14aee8298
buunhut <nhut.nta@gmail.com>
2023-06-21 18:18:01 +0700

    QLHV

---
+++ js/index.js
@@ -0,0 +1,258 @@
+import ChucNang from "./chucnang.js";
+import PerSon, { KhachHang, NhanVien, SinhVien } from "./objects.js";
+import CheckInput from "./validation.js";
+
+let sinhVien = new SinhVien();
+let nhanVien = new NhanVien();
+let khachHang = new KhachHang();
+let chucNang = new ChucNang();
+let checkInput = new CheckInput();
+
+chucNang.goiLocal();
+chucNang.doDuLieuChoSort();
+chucNang.renderGiaoDien();
+
+document.getElementById("themMoi").addEventListener("click", () => {
+  chucNang.tatMoForm();
+  chucNang.resetForm();
+  document.getElementById("sinhVien").classList.add("none");
+  document.getElementById("nhanVien").classList.add("none");
+  document.getElementById("khachHang").classList.add("none");
+  document.getElementById("doiTuong").value = "";
+  document.getElementById("doiTuong").disabled = false;
+  document.getElementById("ma").disabled = false;
+  document.getElementById("ketQua").innerHTML = "";
+});
+document.getElementById("myOverlay").addEventListener("click", () => {
+  chucNang.tatMoForm();
+});
+
+document.getElementById("closer").addEventListener("click", () => {
+  chucNang.tatMoForm();
+});
+
+document.getElementById("doiTuong").addEventListener("change", () => {
+  let mySelect = document.getElementById("doiTuong").value;
+  if (mySelect == "sinh viên") {
+    document.getElementById("sinhVien").classList.remove("none");
+    document.getElementById("nhanVien").classList.add("none");
+    document.getElementById("khachHang").classList.add("none");
+  } else if (mySelect == "nhân viên") {
+    document.getElementById("sinhVien").classList.add("none");
+    document.getElementById("nhanVien").classList.remove("none");
+    document.getElementById("khachHang").classList.add("none");
+  } else if (mySelect == "khách hàng") {
+    document.getElementById("sinhVien").classList.add("none");
+    document.getElementById("nhanVien").classList.add("none");
+    document.getElementById("khachHang").classList.remove("none");
+  } else {
+    document.getElementById("sinhVien").classList.add("none");
+    document.getElementById("nhanVien").classList.add("none");
+    document.getElementById("khachHang").classList.add("none");
+  }
+});
+
+document.getElementById("luuDoiTuongMoi").addEventListener("click", () => {
+  let mySelect = document.getElementById("doiTuong").value;
+  if (mySelect == "") {
+    let valid = true;
+    valid =
+      checkInput.checkMa("ma", "checkMa") &
+      checkInput.checkMa("ten", "checkTen") &
+      checkInput.checkEmail("email", "checkEmail") &
+      checkInput.checkDiaChi("diaChi", "checkDiaChi") &
+      checkInput.checkMa("doiTuong", "checkDoiTuong");
+
+    if (!valid) {
+      return;
+    }
+
+    return;
+  } else {
+    let personInputs = document.querySelectorAll(
+      "#myForm #person input, select "
+    );
+
+    if (mySelect == "sinh viên") {
+      let inputSinhVien = document.querySelectorAll("#myForm #sinhVien input");
+      let allInputs = [...personInputs, ...inputSinhVien];
+
+      for (let inputSinhVien of allInputs) {
+        let { id, value } = inputSinhVien;
+        if (id == "toan" || id == "ly" || id == "hoa") {
+          value = Number(value);
+        } else {
+          value = value.toLowerCase().trim();
+        }
+        sinhVien[id] = value;
+      }
+
+      let valid = true;
+      valid =
+        checkInput.checkMa("ma", "checkMa") &
+        checkInput.checkMa("ten", "checkTen") &
+        checkInput.checkEmail("email", "checkEmail") &
+        checkInput.checkDiaChi("diaChi", "checkDiaChi") &
+        checkInput.checkMa("doiTuong", "checkDoiTuong") &
+        checkInput.checkDiem("toan", "checkDiemToan") &
+        checkInput.checkDiem("ly", "checkDiemLy") &
+        checkInput.checkDiem("hoa", "checkDiemHoa");
+
+      if (!valid) {
+        return;
+      }
+      chucNang.themDoiTuong(sinhVien);
+    } else if (mySelect == "nhân viên") {
+      let inputnhanVien = document.querySelectorAll("#myForm #nhanVien input");
+      let allInputs = [...personInputs, ...inputnhanVien];
+      for (let inputnhanVien of allInputs) {
+        let { id, value } = inputnhanVien;
+        if (id == "soNgayLam" || id == "luongNgay") {
+          value = Number(value);
+        } else {
+          value = value.toLowerCase().trim();
+        }
+        nhanVien[id] = value;
+      }
+      let valid = true;
+      valid =
+        checkInput.checkMa("ma", "checkMa") &
+        checkInput.checkMa("ten", "checkTen") &
+        checkInput.checkEmail("email", "checkEmail") &
+        checkInput.checkDiaChi("diaChi", "checkDiaChi") &
+        checkInput.checkMa("doiTuong", "checkDoiTuong");
+      if (!valid) {
+        return;
+      }
+      chucNang.themDoiTuong(nhanVien);
+    } else if (mySelect == "khách hàng") {
+      let inputKhachHang = document.querySelectorAll(
+        "#myForm #khachHang input"
+      );
+      let allInputs = [...personInputs, ...inputKhachHang];
+      for (let inputKhachHang of allInputs) {
+        let { id, value } = inputKhachHang;
+        if (id == "giaTriHd") {
+          value = Number(value);
+        } else {
+          value = value.toLowerCase().trim();
+        }
+        khachHang[id] = value;
+      }
+      let valid = true;
+      valid =
+        checkInput.checkMa("ma", "checkMa") &
+        checkInput.checkMa("ten", "checkTen") &
+        checkInput.checkEmail("email", "checkEmail") &
+        checkInput.checkDiaChi("diaChi", "checkDiaChi") &
+        checkInput.checkMa("doiTuong", "checkDoiTuong");
+
+      if (!valid) {
+        return;
+      }
+
+      chucNang.themDoiTuong(khachHang);
+    }
+
+    chucNang.luuLocal();
+    chucNang.goiLocal();
+    chucNang.doDuLieuChoSort();
+    chucNang.renderGiaoDien();
+    chucNang.resetForm();
+    document.getElementById("ketQua").innerHTML = "Thêm thành công";
+  }
+});
+
+document.getElementById("capNhatDoiTuong").addEventListener("click", () => {
+  let mySelect = document.getElementById("doiTuong").value;
+  if (mySelect == "") {
+    return;
+  } else {
+    let personInputs = document.querySelectorAll(
+      "#myForm #person input, select "
+    );
+    // console.log(personInputs);
+    if (mySelect == "sinh viên") {
+      let inputSinhVien = document.querySelectorAll("#myForm #sinhVien input");
+      let allInputs = [...personInputs, ...inputSinhVien];
+      for (let inputSinhVien of allInputs) {
+        let { id, value } = inputSinhVien;
+        if (id == "toan" || id == "ly" || id == "hoa") {
+          value = Number(value);
+        } else {
+          value = value.toLowerCase().trim();
+        }
+        sinhVien[id] = value;
+      }
+      chucNang.capNhat(sinhVien);
+    } else if (mySelect == "nhân viên") {
+      let inputnhanVien = document.querySelectorAll("#myForm #nhanVien input");
+      let allInputs = [...personInputs, ...inputnhanVien];
+      for (let inputnhanVien of allInputs) {
+        let { id, value } = inputnhanVien;
+        if (id == "soNgayLam" || id == "luongNgay") {
+          value = Number(value);
+        } else {
+          value = value.toLowerCase().trim();
+        }
+        nhanVien[id] = value;
+      }
+      chucNang.capNhat(nhanVien);
+    } else if (mySelect == "khách hàng") {
+      let inputKhachHang = document.querySelectorAll(
+        "#myForm #khachHang input"
+      );
+      let allInputs = [...personInputs, ...inputKhachHang];
+      for (let inputKhachHang of allInputs) {
+        let { id, value } = inputKhachHang;
+        if (id == "giaTriHd") {
+          value = Number(value);
+        } else {
+          value = value.toLowerCase().trim();
+        }
+        khachHang[id] = value;
+      }
+      chucNang.capNhat(khachHang);
+    }
+  }
+});
+
+document.getElementById("timKiem").addEventListener("keyup", () => {
+  chucNang.timKiemDoiTuong();
+});
+
+document.getElementById("sortDoiTuong").addEventListener("change", () => {
+  let doiTuongCanSort = document.getElementById("sortDoiTuong").value;
+  chucNang.sortDoiTuong(doiTuongCanSort);
+});
+
+window.chinhSua = (ma) => {
+  chucNang.chinhSua(ma);
+};
+window.xoa = (ma) => {
+  chucNang.xoa(ma);
+};
+window.checkMa = (id, thongBao) => {
+  checkInput.checkMa(id, thongBao);
+};
+window.checkTen = (id, thongBao) => {
+  checkInput.checkTen(id, thongBao);
+};
+window.checkEmail = (id, thongBao) => {
+  checkInput.checkEmail(id, thongBao);
+};
+window.checkDiaChi = (id, thongBao) => {
+  checkInput.checkDiaChi(id, thongBao);
+};
+window.checkDoiTuong = (id, thongBao) => {
+  checkInput.checkMa(id, thongBao);
+};
+window.checkDiemToan = (id, thongBao) => {
+  checkInput.checkDiem(id, thongBao);
+};
+window.checkDiemLy = (id, thongBao) => {
+  checkInput.checkDiem(id, thongBao);
+};
+window.checkDiemHoa = (id, thongBao) => {
+  checkInput.checkDiem(id, thongBao);
+};
---
+++ js/validation.js
@@ -0,0 +1,125 @@
+export default class CheckInput {
+  checkMa = (id, thongBao) => {
+    let regexMa = document.getElementById(id).value;
+    if (regexMa == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else {
+      document.getElementById(thongBao).innerHTML = "";
+      return true;
+    }
+  };
+
+  checkTen = (id, thongBao) => {
+    let regexTen = document.getElementById(id).value;
+    if (regexTen == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else {
+      let chiLayChu = /^[\p{L} ]+$/u;
+      let tenHopLe = chiLayChu.test(regexTen);
+      if (tenHopLe) {
+        document.getElementById(thongBao).innerHTML = "";
+        return true;
+      } else {
+        document.getElementById(thongBao).innerHTML =
+          "Vui lòng nhập tên chỉ chứa ký tự chữ";
+        return false;
+      }
+    }
+  };
+
+  checkEmail = (id, thongBao) => {
+    let regexEmail = document.getElementById(id).value;
+    if (regexEmail == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else {
+      let mauRegexEmail =
+        /^[^\s@áàảãạăắằẳẵặâấầẩẫậđéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ]+@[^\s@áàảãạăắằẳẵặâấầẩẫậđéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ]+\.[^\s@áàảãạăắằẳẵặâấầẩẫậđéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ]+$/i;
+      let mailHopLe = mauRegexEmail.test(regexEmail);
+
+      if (mailHopLe == true) {
+        document.getElementById(thongBao).innerHTML = "";
+        return true;
+      } else {
+        document.getElementById(thongBao).innerHTML =
+          "Vui lòng nhập email hợp lệ (info@gmail.com)";
+        return false;
+      }
+    }
+  };
+
+  checkDiaChi = (id, thongBao) => {
+    let regexDiaChi = document.getElementById(id).value;
+    if (regexDiaChi == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else {
+      document.getElementById(thongBao).innerHTML = "";
+      return true;
+    }
+  };
+
+  checkDiem = (id, thongBao) => {
+    let regexDiem = document.getElementById(id).value;
+    if (regexDiem == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else {
+      let chiLaySo = /[0-9]/g;
+      let diemHopLe = chiLaySo.test(regexDiem);
+      if (diemHopLe) {
+        if (regexDiem >= 0 && regexDiem <= 10) {
+          document.getElementById(thongBao).innerHTML = "";
+          return true;
+        } else {
+          document.getElementById(thongBao).innerHTML =
+            "Vui lòng nhập số điểm hợp lệ";
+          return false;
+        }
+      } else {
+        document.getElementById(thongBao).innerHTML = "Vui lòng nhập ký tự số";
+      }
+    }
+  };
+  checkSoNgayLam = (id, thongBao) => {
+    let regexSoNgayLam = document.getElementById(id).value;
+    if (regexSoNgayLam == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else {
+      let chiLaySo = /[0-9]/g;
+      let ngayHopLe = chiLaySo.test(regexSoNgayLam);
+      if (ngayHopLe) {
+        if (regexSoNgayLam >= 0 && regexSoNgayLam <= 30) {
+          document.getElementById(thongBao).innerHTML = "";
+          return true;
+        } else {
+          document.getElementById(thongBao).innerHTML =
+            "Vui lòng nhập số ngày làm hợp lệ";
+          return false;
+        }
+      } else {
+        document.getElementById(thongBao).innerHTML = "Vui lòng nhập ký tự số";
+      }
+    }
+  };
+  checkDiem = (id, thongBao) => {
+    let input = document.getElementById(id).value;
+    let myRegex = /^[0-9]+$/g;
+    let result = myRegex.test(input);
+    if (input == "") {
+      document.getElementById(thongBao).innerHTML = "Vui lòng nhập trường này";
+      return false;
+    } else if (result == false) {
+      document.getElementById(id).value = "";
+      document.getElementById(thongBao).innerHTML = "Chỉ được nhập số";
+    } else if (input > 10) {
+      document.getElementById(thongBao).innerHTML = "Điểm lớn nhất là 10.";
+      return false;
+    } else {
+      document.getElementById(thongBao).innerHTML = "";
+    }
+  };
+}
